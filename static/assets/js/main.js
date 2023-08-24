5273a629cc471deda55e736f7e18215998fb5d3d
LEE TAE GYEONG <126620644+Luriel0228@users.noreply.github.com>
2023-08-24 03:36:39 +0000

    2

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,5 +1,6 @@
 const loginForm = document.getElementById('login-form');
 const signupForm = document.getElementById('signup-form');
+const errorMessageElement = document.getElementById('error-message');
 
 loginForm.addEventListener('submit', async (e) => {
     e.preventDefault();
@@ -8,21 +9,7 @@ loginForm.addEventListener('submit', async (e) => {
 
     // 로그인 처리 로직 (서버와 통신)
     try {
-        const response = await fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        });
-        const data = await response.json();
-
-        if (response.ok) {
-            // 로그인 성공 처리
-            console.log('로그인 성공');
-        } else {
-            console.error('로그인 실패:', data.error);
-        }
+        // ...
     } catch (error) {
         console.error('오류:', error);
     }
@@ -48,7 +35,8 @@ signupForm.addEventListener('submit', async (e) => {
             // 회원가입 성공 처리
             console.log('회원가입 성공');
         } else {
-            console.error('회원가입 실패:', data.error);
+            // 에러 메시지 표시
+            errorMessageElement.textContent = data.error;
         }
     } catch (error) {
         console.error('오류:', error);
