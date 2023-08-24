c6535316178b32bdd8296b8cba6f5b4fb49664e4
LEE TAE GYEONG <126620644+Luriel0228@users.noreply.github.com>
2023-08-24 04:07:31 +0000

    3

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
+import hashlib
+import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-# SQLite3 데이터베이스 생성 및 연결
 conn = sqlite3.connect('users.db', check_same_thread=False)
-conn.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)')
+conn.execute('CREATE TABLE IF NOT EXISTS users (username TEXT, password_salt TEXT, password_hash TEXT)')
+
+def hash_password(password, salt=None, iterations=100000):
+    if salt is None:
+        salt = os.urandom(32)
+
+    hash_result = hashlib.pbkdf2_hmac(
+        'sha256',
+        password.encode('utf-8'),
+        salt,
+        iterations,
+        dklen=128
+    )
+
+    return salt, hash_result
 
 @app.route('/')
 def index():
@@ -17,12 +32,20 @@ def login():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    cursor = conn.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password))
+    cursor = conn.execute('SELECT * FROM users WHERE username = ?', (username,))
     user = cursor.fetchone()
 
     if user:
-        session['username'] = username
-        return redirect(url_for('dashboard'))
+        stored_salt = user[1]
+        stored_hashed_password = user[2]
+
+        _, hashed_password = hash_password(password, salt=stored_salt)
+
+        if hashed_password == stored_hashed_password:
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            return "잘못된 사용자 이름 또는 비밀번호"
     else:
         return "잘못된 사용자 이름 또는 비밀번호"
 
@@ -31,7 +54,8 @@ def signup():
     username = request.form.get('username')
     password = request.form.get('password')
 
-    conn.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+    salt, hashed_password = hash_password(password)
+    conn.execute('INSERT INTO users (username, password_salt, password_hash) VALUES (?, ?, ?)', (username, salt, hashed_password))
     conn.commit()
 
     return redirect(url_for('index'))
